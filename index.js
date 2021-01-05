2b45528d5a46845534a3d5efcf21ff41330126e6
Azatyk <azatuk2005@mail.ru>
2021-01-05 11:31:19 +0600

    feat: add static data

--- index.js
+++ index.js
@@ -4,37 +4,41 @@ const cors = require("cors");
 
 app.use(cors());
 
-const mysql = require("mysql");
+// const mysql = require("mysql");
 
-const db = mysql.createConnection({
-  user: "root",
-  host: "127.0.0.1",
-  password: "root",
-  database: "servers_test_db",
-  socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
-  port: "8889",
-});
+// const db = mysql.createConnection({
+//   user: "root",
+//   host: "127.0.0.1",
+//   password: "root",
+//   database: "servers_test_db",
+//   socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
+//   port: "8889",
+// });
 
 app.get("/select", (req, res) => {
-  db.query("SELECT * FROM cars", (err, result) => {
-    if (err) {
-      console.log(err);
-    }
+  //   db.query("SELECT * FROM cars", (err, result) => {
+  //     if (err) {
+  //       console.log(err);
+  //     }
 
-    res.send(result);
-  });
+  //     res.send(result);
+  //   });
+  res.send([
+    { id: 1, name: "Lamborgini" },
+    { id: 2, name: "Porshe" },
+  ]);
 });
 
-app.get("/insert/:carName", (req, res) => {
-  const carName = req.params.carName;
-  db.query("INSERT INTO cars (name) VALUES (?)", [carName], (err, result) => {
-    if (err) {
-      console.log(err);
-    }
+// app.get("/insert/:carName", (req, res) => {
+//   const carName = req.params.carName;
+//   db.query("INSERT INTO cars (name) VALUES (?)", [carName], (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
 
-    res.send(result);
-  });
-});
+//     res.send(result);
+//   });
+// });
 
 app.listen(3001, () => {
   console.log("Server running on port 3001...");
