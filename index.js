d3fb8fed693ddf265d63b3f864012a2f9984fa45
Azatyk <azatuk2005@mail.ru>
2021-01-07 20:02:36 +0600

    feat: change database wrkjbsoribn

--- index.js
+++ index.js
@@ -4,42 +4,36 @@ const cors = require("cors");
 
 app.use(cors());
 
-// const mysql = require("mysql");
+const mysql = require("mysql");
 
-// const db = mysql.createConnection({
-//   user: "doadmin",
-//   host: "server-test-database-do-user-8396420-0.b.db.ondigitalocean.com",
-//   password: "hxgxim581dbcmvkh",
-//   database: "server-test-database",
-//   port: "25060",
-// });
-
-app.get("/select", (req, res) => {
-  // db.query("SELECT * FROM cars", (err, result) => {
-  //   if (err) {
-  //     console.log(err);
-  //   }
-
-  //   res.send(result);
-  // });
-
-  res.send([
-    { id: 1, name: "Всё" },
-    { id: 2, name: "нормально" },
-    { id: 3, name: "работает" },
-  ]);
+const db = mysql.createConnection({
+  user: "doadmin",
+  host: "server-test-database-do-user-8396420-0.b.db.ondigitalocean.com",
+  password: "hxgxim581dbcmvkh",
+  database: "server-test-database",
+  port: "25060",
 });
 
-// app.get("/insert/:carName", (req, res) => {
-//   const carName = req.params.carName;
-//   db.query("INSERT INTO cars (name) VALUES (?)", [carName], (err, result) => {
-//     if (err) {
-//       console.log(err);
-//     }
+app.get("/select", (req, res) => {
+  db.query("SELECT * FROM cars", (err, result) => {
+    if (err) {
+      console.log(err);
+    }
 
-//     res.send(result);
-//   });
-// });
+    res.send(result);
+  });
+});
+
+app.get("/insert/:carName", (req, res) => {
+  const carName = req.params.carName;
+  db.query("INSERT INTO cars (name) VALUES (?)", [carName], (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+
+    res.send(result);
+  });
+});
 
 app.listen(3001, () => {
   console.log("Server running on port 3001...");
